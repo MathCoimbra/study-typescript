ef2633e4097c8de308e57a6ac4db992f53b7c59d
MathCoimbra <matheuscoimbra35@gmail.com>
2022-07-31 17:59:13 -0300

    adicionado exemplo de type com HTML e generic type
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- type.ts
+++ type.ts
@@ -14,12 +14,31 @@ interface IPessoaFisica extends IPessoa {
   cpf: number
 }
 
-/* Junção das interfaces */
+  /* Junção das interfaces */
 type IMei = IPessoa & IPessoaJuridica
 
-/* Com essa junção todos os dados são requeridos na implementação: */
+  /* Com essa junção todos os dados são requeridos na implementação: */
 const pj: IMei = {
   nome: 'MathCoimbra',
   tipo: 'jurídica',
   cnpj: 45789142000121 
 }
+
+/* Exemplo de type com elemento HTML */
+
+const input = document.getElementById('input') as HTMLInputElement
+
+input.addEventListener('input', (event) => {
+  const i = event.currentTarget as HTMLInputElement
+  console.log(i.value)
+})
+
+/* Generic type */
+
+  /* <Generic> - definido que os parâmetros dessa função podem ser de qualquer tipo */
+  /* any[] - significa que esse array pode ser de qualquer tipo */
+function getItem<Generic>(array: any[], valor: Generic){
+  return array.map(item => item + valor)
+}
+
+getItem([1,2,3,4], 'a')
