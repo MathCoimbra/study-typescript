22678f146d16f9f25f59a555150dfb287e3150ab
MathCoimbra <matheuscoimbra35@gmail.com>
2022-07-30 21:25:36 -0300

    adicionado validações de erros que o typescript informa
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

---
+++ validacoes.ts
@@ -0,0 +1,21 @@
+/* Abaixo é apresentado uma validação de erro de tipo ao descomentar o código, se espera um número e foi atribuída uma string */
+
+/* function soma(a: number, b:number){
+  return a + b
+}
+
+soma(1, 'a') */
+
+/* No caso abaixo é uma propriedade escrita que não setada na criação da interface */
+
+/* 
+interface IAnimal {
+  nome: string
+  tipo: 'terrestre' | 'aquático'
+}
+
+const animal: IAnimal = {
+  nome: 'Elefante',
+  tipo: 'voador'
+}
+ */
